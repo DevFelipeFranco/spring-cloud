f72244a39e030ff824d14e865b1a738680c120e2
Felipe Franco <devfelipefranco@gmail.com>
2021-09-16 22:54:06 -0500

    Se crea configuracion para el microservicio de product

--- service-customer/src/main/resources/data.sql
+++ service-customer/src/main/resources/data.sql
@@ -1,2 +1,12 @@
-INSERT INTO customers (id,number_id,first_name,last_name,email)
-    VALUES(1,'1','Jorge','Perez','Jorge@gmail.com');
+CREATE TABLE customers
+(
+    id         INT NOT NULL AUTO_INCREMENT,
+    number_id  INT,
+    first_name VARCHAR(60),
+    last_name  VARCHAR(60),
+    email      VARCHAR(60),
+    CONSTRAINT CUSTOMERS_ID_PK PRIMARY KEY (ID)
+);
+
+INSERT INTO customers (id, number_id, first_name, last_name, email)
+VALUES (1, '1', 'Jorge', 'Perez', 'Jorge@gmail.com');
--- service-product/src/main/resources/data.sql
+++ service-product/src/main/resources/data.sql
@@ -1,12 +1,33 @@
-INSERT INTO categories (id,name) VALUES (1,'Hombre');
-INSERT INTO categories (id,name) VALUES (2,'Mujer');
-INSERT INTO categories (id,name) VALUES (3,'Niñ@');
+CREATE TABLE CATEGORIES
+(
+    id   INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50),
+    CONSTRAINT CATEGORIES_ID_PK PRIMARY KEY (ID)
+);
 
-INSERT INTO products (id,name,price,stock,category_id)
-VALUES (1,'Guayos',10,10,1);
+CREATE TABLE PRODUCTS
+(
+    id          INT NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(60),
+    price       INT,
+    stock       INT,
+    category_id INT,
+    CONSTRAINT PRODUCTS_ID_PK PRIMARY KEY (ID),
+    CONSTRAINT PRODUCTS_CATEGORY_FK FOREIGN KEY (category_id) REFERENCES CATEGORIES (id)
+);
 
-INSERT INTO products (id,name,price,stock,category_id)
-VALUES (2,'Bolso',20,20,2);
+INSERT INTO categories (id, name)
+VALUES (1, 'Hombre');
+INSERT INTO categories (id, name)
+VALUES (2, 'Mujer');
+INSERT INTO categories (id, name)
+VALUES (3, 'Niñ@');
 
-INSERT INTO products (id,name,price,stock,category_id)
-VALUES (3,'Balon',30,30,3);
+INSERT INTO products (id, name, price, stock, category_id)
+VALUES (1, 'Guayos', 10, 10, 1);
+
+INSERT INTO products (id, name, price, stock, category_id)
+VALUES (2, 'Bolso', 20, 20, 2);
+
+INSERT INTO products (id, name, price, stock, category_id)
+VALUES (3, 'Balon', 30, 30, 3);
